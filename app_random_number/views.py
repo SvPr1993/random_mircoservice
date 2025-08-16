62c3a928dbe87b9bc7f43f55e2be33abfef320f7
SvPr1993 <mrtventana@gmail.com>
2025-08-16 11:59:25 +0300

    random_microservice

--- app_random_number/views.py
+++ app_random_number/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from app_random_number.service import random_num
 
+
 class Number(APIView):
     def get(self, request):
         int_num = random_num()
