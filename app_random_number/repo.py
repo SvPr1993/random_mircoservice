fc81c32b5dcf06f1c5f423cc04e01d3d3f0d4f5a
SvPr1993 <mrtventana@gmail.com>
2025-08-17 10:21:26 +0300

    random_microservice

--- app_random_number/repo.py
+++ app_random_number/repo.py
@@ -1,6 +1,11 @@
 import random
-def return_num():
-    num_random = random.randint(1, 100)
-    return num_random
 
-#Сделать рандом усложненный рандом, установить время ответа сайта в милисекундах
+def return_num():
+    choice_int = ""
+    for x in range(12):
+        choice_int = choice_int + random.choice(list("123456789"))
+    return (choice_int)
+
+# Сделать рандом усложненный рандом, установить время ответа сайта в милисекундах
+# num_random = random.randint(1, 100)
+# return num_random
--- app_random_number/views.py
+++ app_random_number/views.py
@@ -2,10 +2,21 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from app_random_number.service import random_num
-
+import requests
+import time
 
 class Number(APIView):
     def get(self, request):
         int_num = random_num()
         json_anwser = {"number": int_num}
         return Response(json_anwser)
+
+
+class TimeCount(APIView):
+    def get(self, request):
+        start_time = time.time()
+        response = requests.get("http://127.0.0.1:3333/")
+        end_time = time.time()
+        response_time_ms = (end_time - start_time) * 1000
+        print(f"Время ответа: {response_time_ms:.2f} мс")
+
